44d63fbe7d7f50ebf71edc2afe0ca4b39d3068a3
Sergio Cardoso <desendata@gmail.com>
2022-08-29 23:55:16 -0300

    config. database knexfile

--- knexfile.js
+++ knexfile.js
@@ -6,12 +6,16 @@
 module.exports = {
 
   development: {
-    client: 'pg',
+    //client: 'pg',
+      client: 'pg',
     connection: {
-      database: "phoenix_crm",
-      user: "postgres",
-      password: "adm@#$"
-    },
+//      database: "phoenix_crm",
+//      user: "postgres",
+//      password: "adm@#$"
+      database: "d8scmptcj13n6b",
+      user: "dgcbqimbztpnlg",
+      password: "5badf414c7bb78702dd0a0a78ed63b0f60b956e48cd84899090753bd6070cf7d"
+},
     migrations: {
       tablename: 'knex_migrations',
       directory: `${__dirname}/src/database/migrations`
